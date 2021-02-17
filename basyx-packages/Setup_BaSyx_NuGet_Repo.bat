e566622593aafac557964fc3f8d3c74936772158
Constantin Ziesche <constantin.ziesche@bosch.com>
2021-02-17 17:21:37 +0100

    Redundant NuGet package repo removed

--- basyx-packages/Setup_BaSyx_NuGet_Repo.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET currentPath=%~dp0
-echo New NuGet-Repository Path: %currentPath:~0,-1%
-setx BASYX_REPO %currentPath:~0,-1%
-dotnet nuget add source %currentPath:~0,-1% --name BaSyx.Packages
