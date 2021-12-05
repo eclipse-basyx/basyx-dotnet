4bb59775f435007f4ecc9d2b9d781dfaec17ec85
Constantin Ziesche <constantin.ziesche@bosch.com>
2021-12-05 11:44:50 +0100

    Change mirror to the use of submodules of the official BaSyx git repo

--- Setup_BaSyx.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET currentPath=%~dp0
-echo New NuGet-Repository Path: %currentPath:~0,-1%\basyx-packages
-setx BASYX_REPO %currentPath:~0,-1%\basyx-packages
-mkdir basyx-packages
-dotnet nuget add source %currentPath:~0,-1%\basyx-packages --name BaSyx.Packages
