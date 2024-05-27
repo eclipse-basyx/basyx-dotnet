6942101862f23abb52cd894217ea5929cc8b5acc
Constantin Ziesche <constantin.ziesche@bosch.com>
2024-05-27 14:34:42 +0200

    Added mlp value support

--- basyx-dotnet-sdk/BaSyx.Models/AssetAdministrationShell/Implementations/MultiLanguageProperty.cs
+++ basyx-dotnet-sdk/BaSyx.Models/AssetAdministrationShell/Implementations/MultiLanguageProperty.cs
@@ -13,14 +13,12 @@ using System.Runtime.Serialization;
 namespace BaSyx.Models.AdminShell
 {
     [DataContract]
-    public class MultiLanguageProperty : SubmodelElement, IMultiLanguageProperty
+    public class MultiLanguageProperty : SubmodelElement<MultiLanguagePropertyValue>, IMultiLanguageProperty
     {
         [DataMember(EmitDefaultValue = false, IsRequired = false, Name = "modelType")]
         public override ModelType ModelType => ModelType.MultiLanguageProperty;
         [DataMember(EmitDefaultValue = false, IsRequired = false, Name = "valueId")]
         public IReference ValueId { get; set; }
-        [DataMember(EmitDefaultValue = false, IsRequired = false, Name = "value")]
-        public new LangStringSet Value { get; set; }
         public MultiLanguageProperty(string idShort) : base(idShort)
         {
 
