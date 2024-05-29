ec2c83a0c0a9907c003ab46bba86bc3e4e1a4b11
Constantin Ziesche <constantin.ziesche@bosch.com>
2024-05-29 09:41:47 +0200

    Fixed ValueOnly Converter for operations

--- basyx-dotnet-sdk/BaSyx.Models/Extensions/JsonConverters/SubmodelElementContainerValueOnlyConverter.cs
+++ basyx-dotnet-sdk/BaSyx.Models/Extensions/JsonConverters/SubmodelElementContainerValueOnlyConverter.cs
@@ -34,6 +34,9 @@ namespace BaSyx.Models.Extensions
 
             foreach (var smElement in value)
             {
+                if (smElement.ModelType == ModelType.Operation)
+                    continue;
+
                 writer.WritePropertyName(smElement.IdShort);
                 var valueScope = smElement.GetValueScope().Result;
                 JsonSerializer.Serialize<ValueScope>(writer, valueScope, new JsonSerializerOptions()
@@ -45,137 +48,7 @@ namespace BaSyx.Models.Extensions
                             SerializationOption = SerializationOption.ValueOnly
                         }, jsonOptions: _options)
                     }
-                });
-
-       //         switch (smElement.ModelType.Type)
-       //         {
-       //             case ModelTypes.SubmodelElementCollection:
-       //                 {
-       //                     ISubmodelElementCollection submodelElementCollection = smElement.Cast<ISubmodelElementCollection>();
-       //                     writer.WritePropertyName(submodelElementCollection.IdShort);
-       //                     Write(writer, submodelElementCollection.Value.Value, options);
-       //                     break;
-       //                 }
-       //             case ModelTypes.SubmodelElementList:
-       //                 {
-       //                     ISubmodelElementList submodelElementList = smElement.Cast<ISubmodelElementList>();
-       //                     writer.WritePropertyName(submodelElementList.IdShort);
-       //                     writer.WriteStartArray();
-       //                     foreach (var element in submodelElementList.Value.Value)
-       //                     {
-       //                         if(element.ModelType == ModelType.Property)
-       //                         {
-       //                             JsonSerializer.Serialize(writer, element.Value, options);
-       //                         }
-       //                     }
-       //                     writer.WriteEndArray();
-       //                     break;
-       //                 }
-       //             case ModelTypes.RelationshipElement:
-       //                 {
-       //                     //IRelationshipElement relationshipElement = smElement.Cast<IRelationshipElement>();
-       //                     //writer.WritePropertyName(relationshipElement.IdShort);
-       //                     //writer.WriteStartObject();
-       //                     //writer.WriteString("first", relationshipElement.First.ToStandardizedString());
-       //                     //writer.WriteString("second", relationshipElement.Second.ToStandardizedString());
-       //                     //writer.WriteEndObject();
-       //                     break;
-       //                 }
-       //             case ModelTypes.AnnotatedRelationshipElement:
-       //                 {
-       //                     //IAnnotatedRelationshipElement annotatedRelationshipElement = smElement.Cast<IAnnotatedRelationshipElement>();
-       //                     //writer.WritePropertyName(annotatedRelationshipElement.IdShort);
-       //                     //writer.WriteStartObject();
-       //                     //writer.WriteString("first", annotatedRelationshipElement.First.ToStandardizedString());
-       //                     //writer.WriteString("second", annotatedRelationshipElement.Second.ToStandardizedString());
-       //                     //writer.WritePropertyName("annotations");
-       //                     //Write(writer, annotatedRelationshipElement.Annotations, options);
-       //                     //writer.WriteEndObject();
-       //                     break;
-       //                 }
-       //             case ModelTypes.Property:
-       //                 {
-       //                     IProperty property = smElement.Cast<IProperty>();
-       //                     var valueScope = property.GetValueScope<PropertyValue>();
-       //                     if(valueScope != null && valueScope.Value != null)
-       //                     {
-       //                         writer.WritePropertyName(property.IdShort);
-       //                         var jValue = JsonValue.Create(valueScope.Value.Value);
-       //                         writer.WriteRawValue(jValue.ToString());
-       //                     }                           
-       //                     break;
-       //                 }
-       //             case ModelTypes.File:
-       //                 {
-       //                     //IFileElement file = smElement.Cast<IFileElement>();
-       //                     //writer.WritePropertyName(file.IdShort);
-       //                     //writer.WriteStartObject();
-       //                     //writer.WriteString("contentType", file.ContentType);
-       //                     //writer.WriteString("value", file.Value);
-       //                     //writer.WriteEndObject();
-       //                     break;
-       //                 }
-       //             case ModelTypes.Blob:
-       //                 {
-       //                     //IBlob blob = smElement.Cast<IBlob>();
-       //                     //writer.WritePropertyName(blob.IdShort);
-       //                     //writer.WriteStartObject();
-       //                     //writer.WriteString("contentType", blob.ContentType);
-       //                     //writer.WriteString("value", blob.Value);
-       //                     //writer.WriteEndObject();
-       //                     break;
-       //                 }
-       //             case ModelTypes.ReferenceElement:
-       //                 {
-       //                     IReferenceElement referenceElement = smElement.Cast<IReferenceElement>();
-       //                     writer.WriteString(referenceElement.IdShort, referenceElement.Value.Value.ToStandardizedString());
-       //                     break;
-       //                 }
-       //             case ModelTypes.MultiLanguageProperty:
-       //                 {
-       //                     IMultiLanguageProperty multiLanguageProperty = smElement.Cast<IMultiLanguageProperty>();
-							//var mlpValue = multiLanguageProperty.GetValueScope<MultiLanguagePropertyValue>();
-							//writer.WritePropertyName(multiLanguageProperty.IdShort);
-       //                     writer.WriteStartArray();
-       //                     foreach (var langPair in mlpValue.Value)
-       //                     {
-       //                         writer.WriteStartObject();
-       //                         writer.WriteString(langPair.Language, langPair.Text);
-       //                         writer.WriteEndObject();
-       //                     }
-       //                     writer.WriteEndArray();
-       //                     break;
-       //                 }
-       //             case ModelTypes.Range:
-       //                 {
-       //                     IRange range = smElement.Cast<IRange>();
-       //                     var valueScope = range.GetValueScope<RangeValue>();
-       //                     writer.WritePropertyName(range.IdShort);
-       //                     writer.WriteStartObject();
-       //                     var jValueMin = JsonValue.Create(valueScope.Min.Value);
-       //                     var jValueMax = JsonValue.Create(valueScope.Max.Value);
-       //                     writer.WritePropertyName("min");
-       //                     writer.WriteRawValue(jValueMin.ToString());
-       //                     writer.WritePropertyName("max");
-       //                     writer.WriteRawValue(jValueMax.ToString());
-       //                     writer.WriteEndObject();
-       //                     break;
-       //                 }
-       //             case ModelTypes.Entity:
-       //                 {
-       //                     //IEntity entity = smElement.Cast<IEntity>();
-       //                     //writer.WritePropertyName(entity.IdShort);
-       //                     //writer.WriteStartObject();
-       //                     //writer.WritePropertyName("statements");
-       //                     //Write(writer, entity.Statements, options);
-       //                     //writer.WriteString("entityType", entity.EntityType.ToString());
-       //                     //writer.WriteString("globalAssetId", entity.GlobalAssetId.Id);
-       //                     //writer.WriteEndObject(); 
-       //                     break;
-       //                 }
-       //             default:
-       //                 break;
-       //         }
+                });   
             }
 
             writer.WriteEndArray();
