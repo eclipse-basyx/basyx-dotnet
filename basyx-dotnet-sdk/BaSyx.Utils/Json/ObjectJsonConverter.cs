b3ab14bd7bcb13adb65e5670c87bf1ae02c03775
Constantin Ziesche <constantin.ziesche@bosch.com>
2024-08-28 13:43:19 +0200

    Added Header

--- basyx-dotnet-sdk/BaSyx.Utils/Json/ObjectJsonConverter.cs
+++ basyx-dotnet-sdk/BaSyx.Utils/Json/ObjectJsonConverter.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*******************************************************************************
+* Copyright (c) 2024 Bosch Rexroth AG
+* Author: Constantin Ziesche (constantin.ziesche@bosch.com)
+*
+* This program and the accompanying materials are made available under the
+* terms of the MIT License which is available at
+* https://github.com/eclipse-basyx/basyx-dotnet/blob/main/LICENSE
+*
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using System.Text.Json;
