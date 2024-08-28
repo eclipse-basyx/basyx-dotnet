d03974664b69cc4cb5755593073d90d2060c72cd
Constantin Ziesche <constantin.ziesche@bosch.com>
2024-08-28 13:30:50 +0200

    Updated License Header

--- basyx-dotnet-components/BaSyx.Common.UI/CommonUIConfigureOptions.cs
+++ basyx-dotnet-components/BaSyx.Common.UI/CommonUIConfigureOptions.cs
@@ -1,5 +1,5 @@
 ﻿/*******************************************************************************
-* Copyright (c) 2023 Bosch Rexroth AG
+* Copyright (c) 2024 Bosch Rexroth AG
 * Author: Constantin Ziesche (constantin.ziesche@bosch.com)
 *
 * This program and the accompanying materials are made available under the
