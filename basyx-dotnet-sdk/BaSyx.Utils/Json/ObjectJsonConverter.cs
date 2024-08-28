bb184dcddf6621c76d7473a9593058019db20427
Constantin Ziesche <constantin.ziesche@bosch.com>
2024-08-28 13:18:26 +0200

    Updated implementation

--- basyx-dotnet-sdk/BaSyx.Utils/Json/ObjectJsonConverter.cs
+++ basyx-dotnet-sdk/BaSyx.Utils/Json/ObjectJsonConverter.cs
@@ -15,70 +15,75 @@ namespace BaSyx.Utils.Json
 				writer.WriteEndObject();
 			}
 			else
-			{
 				JsonSerializer.Serialize(writer, value, value.GetType(), options);
-			}
 		}
 
 		public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 		{
 			switch (reader.TokenType)
-			{
-				case JsonTokenType.Null:
-					return null;
-				case JsonTokenType.False:
-					return false;
-				case JsonTokenType.True:
-					return true;
+			{				
+                case JsonTokenType.True:
+                    return true;
+                case JsonTokenType.False:
+					return false;				
 				case JsonTokenType.String:
 					return reader.GetString();
 				case JsonTokenType.Number:
 					{
-						if (reader.TryGetInt32(out var i))
-							return i;
-						if (reader.TryGetInt64(out var l))
-							return l;
-						else if (reader.TryGetDouble(out var d))
-							return d;
-						using var doc = JsonDocument.ParseValue(ref reader);
-						return doc.RootElement.Clone();
+						if (reader.TryGetInt32(out int iValue))
+							return iValue;
+						if (reader.TryGetInt64(out long i64Value))
+                            return i64Value;
+						else if (reader.TryGetDouble(out double dValue))
+							return dValue;
+						else
+						{
+							using (var jDocument = JsonDocument.ParseValue(ref reader))
+                            {
+                                return jDocument.RootElement.Clone();
+                            };                           
+                        }						
 					}
-				case JsonTokenType.StartArray:
+                case JsonTokenType.StartObject:
+                    Dictionary<string, object> objDict = new Dictionary<string, object>();
+                    while (reader.Read())
+                    {
+                        switch (reader.TokenType)
+                        {
+                            case JsonTokenType.EndObject:
+                                return objDict;
+                            case JsonTokenType.PropertyName:
+                                string keyName = reader.GetString();
+                                reader.Read();
+                                var obj = Read(ref reader, typeof(object), options);
+                                objDict.Add(keyName, obj);
+                                break;
+                            default:
+                                throw new JsonException();
+                        }
+                    }
+                    throw new JsonException();
+                case JsonTokenType.StartArray:
 					{
-						var list = new List<object>();
+                        List<object> arrayList = new List<object>();
 						while (reader.Read())
 						{
 							switch (reader.TokenType)
 							{
 								default:
-									list.Add(Read(ref reader, typeof(object), options));
+									var obj = Read(ref reader, typeof(object), options);
+                                    arrayList.Add(obj);
 									break;
 								case JsonTokenType.EndArray:
-									return list;
+									return arrayList;
 							}
 						}
 						throw new JsonException();
-					}
-				case JsonTokenType.StartObject:
-					var dict = new Dictionary<string, object>();
-					while (reader.Read())
-					{
-						switch (reader.TokenType)
-						{
-							case JsonTokenType.EndObject:
-								return dict;
-							case JsonTokenType.PropertyName:
-								var key = reader.GetString();
-								reader.Read();
-								dict.Add(key, Read(ref reader, typeof(object), options));
-								break;
-							default:
-								throw new JsonException();
-						}
-					}
-					throw new JsonException();
-				default:
-					throw new JsonException(string.Format("Unknown token {0}", reader.TokenType));
+					}				
+                case JsonTokenType.Null:
+                    return null;
+                default:
+					throw new JsonException($"TokenType unknown: {reader.TokenType}");
 			}
 		}		
 	}
