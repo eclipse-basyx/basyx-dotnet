fafde581945cb8118cbf10893efb7621a3c95c62
constantinziesche <73098116+constantinziesche@users.noreply.github.com>
2025-01-28 15:30:07 +0100

    Update Clear_Local_BaSyx_NuGet_Cache.bat
    See issue #1

--- Clear_Local_BaSyx_NuGet_Cache.bat
+++ Clear_Local_BaSyx_NuGet_Cache.bat
@@ -1,3 +1,5 @@
-cd /D %USERPROFILE%\.nuget\packages
-for /f %%i in ('dir /a:d /s /b basyx.*') do rd /s /q %%i
+IF EXIST %USERPROFILE%\.nuget\packages (
+  cd /D %USERPROFILE%\.nuget\packages
+  for /f %%i in ('dir /a:d /s /b basyx.*') do rd /s /q %%i
+)
 exit 0
